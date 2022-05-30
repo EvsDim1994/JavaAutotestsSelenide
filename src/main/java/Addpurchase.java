97d12d2dcb99d3f635b485aa039b501f7e7756a2
EvsDim1994 <evsyuckov.dmitry@yandex.ru>
2022-05-30 09:44:02 +0300

    Новый коммит

--- src/main/java/Addpurchase.java
+++ src/main/java/Addpurchase.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
-/*Добавление товара*/
+/*Добавление женского топика*/
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class Addpurchase {
