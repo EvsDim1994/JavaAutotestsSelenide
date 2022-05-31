dc8b98a16b7769a3a981ad36ae0cc0ecb0a1104f
EvsDim1994 <evsyuckov.dmitry@yandex.ru>
2022-05-31 12:28:00 +0300

    Добавление Blouse + проверка цены

---
+++ src/main/java/Addpurchase2.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+//Добавление Blouse + проверка цены
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class Addpurchase2 extends Hooks {
+    @Test
+    public void test2() {
+        List<SelenideElement> list1 = $$x("//div[@id='block_top_menu']/ul/li/a");
+        // Клик по первому элементу списка
+        list1.get(0).click();
+        $x("//span[text()='Women']").shouldBe(Condition.visible);
+        $x("//div[@id='subcategories']//a[@title='Tops']").click();
+        $x("//h2[contains(text(),'Tops')]").shouldBe(Condition.visible);
+        List<SelenideElement> listProducts = $$x("//div[@class='product-container']");
+        List<SelenideElement> price = $$x("//span[contains(text(),'$27.00')]");
+        if (price.get(1).exists()==true) {
+            System.out.println("Цена соотвествует $27.00");
+            listProducts.get(1).hover();
+            listProducts.get(1).$x(".//a[@title='Add to cart']").click();
+        }
+        else {
+            System.out.println("Цена не соотвесвтует $27.00, товар не добавлен");
+        }
+    }
+}
