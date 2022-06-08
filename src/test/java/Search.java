55fdc9e92adbc12da5f1deadfe995b8747881f1b
EvsDim1994 <evsyuckov.dmitry@yandex.ru>
2022-06-08 13:05:34 +0300

    Новый коммит

---
+++ src/test/java/Search.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class Search extends Hooks {
+    @Test
+    public void test4() {
+          $x("//input[@name='search_query']").sendKeys("dress");
+          $x("//button[@name='submit_search']").click();
+          String resultsearch =  $x("//span[@class='heading-counter']").shouldBe(Condition.visible).getText();
+          if (resultsearch.contains("7 results have been found")){
+              System.out.println(resultsearch);
+          }
+          else {
+              System.out.println("Найдено другое количество");
+          }
+          $x("//span[contains(text(),'dress')]").shouldBe(Condition.visible);
+          $x("//div[@id='uniform-selectProductSort']").click();
+          $x("//option[@value='price:desc']").click();
+          String text = "Price: Highest first";
+          $x("//option[@selected='selected']").shouldHave(Condition.text(text));
+
+    }
+}
