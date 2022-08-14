d9cdc647dd5256fae575b4335b38c6d254616c2a
EvsDim1994 <evsyuckov.dmitry@yandex.ru>
2022-08-14 11:52:19 +0300

    Обновленный коммит от 14.08.

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -2,25 +2,17 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 public class Hooks {
-
-    @BeforeAll
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/79884/JavaAutotestsSelenide2/chromedriver.exe");
-    }
 
     @BeforeEach
     public void tearDown() {
         Configuration.reportsFolder = "ScreenShots";
         Configuration.timeout = 20000;
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        WebDriverRunner.setWebDriver(webDriver);
         Selenide.open("http://automationpractice.com/index.php");
+        Selenide.webdriver().driver().getWebDriver().manage().window().maximize();
     }
 
   @AfterEach
